2db95950b1cd89cfe99ee23f503bd1c8f24735f0
YANG-git-1 <483242530@qq.com>
2022-05-03 16:45:15 +0800

    main

--- text.lisp
+++ text.lisp
@@ -1,31 +1,21 @@
-(defun occurrences (x)
+(defun commpress (x) 
+    (do ((len (length x) (- len 1)))
+        ((equal len 0) x)
+        (if (listp (last x))
+            (if (equal (car x) (nth 1 (last x)))
+                (setf (nth 1 (last x)) (+ 1 (nth 1 (last x))) x (cdr x)))
+                (if (equal (car x) (nth 1 x))
+                    (setf x (cons (cdr x) (list (cons (car x) '1))))
+                    (setf x (cons (cdr x) (car x)))
+                )
 
-        (if (listp (car x))
-            x
-            (if (member (list (cons (car x) )) x)
-                (progn 
-                (setf (cdr (assoc (car x) x)) (+ (cdr (assoc (car x) x)) 1)) (occurrences (cdr x ) ))
-                (progn 
-                (setf x (append x (list (cons (car x) '1)) ))  (occurrences (cdr x) ) )
             )
+            (if (equal (car x) (nth 1 x))
+                    (setf x (cons (cdr x) (list (cons (car x) '1))))
+                    (setf x (cons (cdr x) (car x)))
+                )
         )
-)
-(princ (occurrences '(b a a a a)  ))
-
-;; (princ (copy-tree '(b a a a )))
-
-;; (princ (nth 1 (car '(a (a .1) (b .2)))))
+    )   
 
 
-;; (princ (append '((a . 1) (b . 2)) (cons (car '(1 2) ) '1) ))
-
-;; (setf test1 '((a . 1) (b . 2) (c . 3)))
-;; (setf avv '(q w e r))
-;; (princ (assoc 'a test1))
-;; (princ (assoc '2 test1))
-;; (setf (nth 0 test1) (cons (car avv) (cdr (nth 0 test1))))
-;; (princ test1)
-;; (princ test2)
-;; (princ (car test1))
-;; (princ (cdr (car test1)))
-;; (princ (sort '( 1 1 2 2 3) #'>) )
+(commpress '(1 0 0 0 1 1 1 2 0 1 1 ))
